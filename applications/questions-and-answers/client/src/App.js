b3e0087449022aeac6e81ecd925dc1419c8cabe0
Paulo Queiroz <phqs.phqs@gmail.com>
2020-07-02 16:39:06 -0300

    add initial project structure

--- applications/questions-and-answers/client/src/App.js
+++ applications/questions-and-answers/client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import data from './data/data';
 
 function App() {
   return (
@@ -16,8 +17,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
+        {data.toString()}
       </header>
     </div>
   );
---
+++ applications/questions-and-answers/client/src/data/data.js
@@ -0,0 +1,60 @@
+const data = [
+    {
+        "_id": "5efe362b99b9d2fb139794b1",
+        "technology": "Emergent",
+        "picture": "http://placehold.it/32x32",
+        "isActive": false,
+        "description": "Esse officia nisi et exercitation esse culpa magna nostrud reprehenderit incididunt minim enim ea enim. Culpa aute sint ad amet proident Lorem. Ipsum laboris Lorem do vel ?",
+        "answer": "Do cillum deserunt labore cillum culpa. Incididunt reprehenderit proident ipsum aliqua deserunt tempor Lorem exercitation non sint cillum. Mollit sunt ea in culpa officia aliquip aute. Ipsum ex excepteur ullamco qui anim esse consectetur in mollit qui. Anim aliquip voluptate Lorem ea enim aute. Id aliqua duis irure ea eu.\r\nPariatur cillum veniam id aliquip culpa minim. Eiusmod ex occaecat elit dolor. Consequat pariatur quis veniam ex et magna mollit Lorem sunt incididunt minim adipisicing. Minim ad non adipisicing aliqua velit dolore laborum velit do ullamco. Consectetur Lorem nulla nulla ad ex non ea. Occaecat mollit est voluptate et.\r\n"
+    },
+    {
+        "_id": "5efe362b890fbfb1b634b163",
+        "technology": "Zilladyne",
+        "picture": "http://placehold.it/32x32",
+        "isActive": true,
+        "description": "Sit esse ut veniam proident minim ad nulla ut anim veniam commodo qui. Qui deserunt do fugiat irure incididunt labore commodo culpa ut reprehenderit ut labore aliqua exce ?",
+        "answer": "Reprehenderit consectetur deserunt excepteur minim commodo ea laborum adipisicing ut. Eu eu dolore dolor et id magna elit et commodo ea amet aute. Nulla dolore cillum eu nostrud voluptate cillum est veniam aute elit. Minim duis elit deserunt laborum ut incididunt ut velit pariatur consectetur. In cillum esse ea deserunt velit cupidatat nisi. Consectetur ut eu amet labore adipisicing laboris ex eiusmod labore quis culpa excepteur amet. Veniam cillum occaecat qui quis non voluptate velit laborum minim aliqua sint.\r\nOccaecat cillum consectetur deserunt duis dolor magna eu. Adipisicing mollit cupidatat id anim ullamco dolore voluptate cupidatat enim deserunt. Ipsum velit ut duis laboris nostrud quis.\r\n"
+    },
+    {
+        "_id": "5efe362b252faa3f644a4c96",
+        "technology": "Lexicondo",
+        "picture": "http://placehold.it/32x32",
+        "isActive": true,
+        "description": "Id eu qui fugiat sint. Nulla Lorem fugiat cillum in dolor. Amet esse veniam elit elit ea amet incididunt mollit nulla culpa tempor fugiat.\r\n ?",
+        "answer": "Ullamco aute ut dolor pariatur fugiat cillum ex sunt. Eiusmod ad veniam sit deserunt laborum fugiat. Proident fugiat ipsum sit laboris ullamco adipisicing. Dolor ad elit nulla dolor.\r\nLorem incididunt esse aliqua labore cupidatat incididunt minim. Eu duis duis officia fugiat consequat pariatur enim et in eiusmod adipisicing. Elit dolore ullamco aute excepteur. Amet fugiat ut sit do adipisicing commodo. Officia deserunt elit sunt ad dolore proident duis cupidatat ipsum sint.\r\n"
+    },
+    {
+        "_id": "5efe362b8ffeddf0840ffe53",
+        "technology": "Grok",
+        "picture": "http://placehold.it/32x32",
+        "isActive": true,
+        "description": "Ullamco proident consequat ad ex sit esse cupidatat. Aute officia culpa duis proident ex id tempor eu. Consectetur voluptate labore sit eu nisi velit veniam sit incididun ?",
+        "answer": "Lorem duis id dolore deserunt consequat. Magna commodo est irure esse sunt sunt culpa consequat. Cillum cupidatat est anim ea laboris ipsum qui commodo ex. Consectetur aliqua sunt eu duis aute dolor.\r\nDolore exercitation culpa cupidatat tempor exercitation ad in anim dolor eu ea. Labore voluptate duis excepteur amet adipisicing laboris laborum voluptate fugiat. Nulla aute excepteur deserunt aliquip elit. Elit ut deserunt ut laborum esse excepteur officia eiusmod.\r\n"
+    },
+    {
+        "_id": "5efe362bd722ce698e4df214",
+        "technology": "Enormo",
+        "picture": "http://placehold.it/32x32",
+        "isActive": true,
+        "description": "Mollit sint do officia deserunt amet quis dolore dolore nostrud. Id velit exercitation deserunt sunt voluptate sit. Dolor cillum ea nisi reprehenderit anim Lorem enim dol ?",
+        "answer": "Excepteur veniam elit id anim eu ipsum esse. Veniam proident cupidatat mollit elit deserunt nostrud ipsum enim voluptate id exercitation aliquip nostrud. Id mollit in minim veniam ipsum consectetur enim sunt irure exercitation proident. Sunt cupidatat irure veniam ullamco.\r\nUt ipsum deserunt minim ullamco exercitation. Officia non officia fugiat nulla ipsum ut tempor dolor. Laborum enim Lorem eu consequat cupidatat commodo sit enim id officia ut anim deserunt culpa. Consequat consectetur magna eiusmod non laboris eiusmod ex duis nulla cillum dolor ex consequat. Id magna fugiat nulla magna consectetur sunt amet mollit voluptate velit ipsum dolor reprehenderit.\r\n"
+    },
+    {
+        "_id": "5efe362b02bfc6c594fafa61",
+        "technology": "Printspan",
+        "picture": "http://placehold.it/32x32",
+        "isActive": true,
+        "description": "Nisi non consectetur Lorem occaecat sunt ullamco pariatur adipisicing proident amet quis non. Exercitation cupidatat nulla amet ex ipsum eiusmod dolore dolore id. Mollit  ?",
+        "answer": "Tempor fugiat culpa ad esse eiusmod occaecat ea commodo. In irure fugiat laborum mollit proident elit ipsum sunt velit id ad Lorem. Eu ea magna deserunt occaecat eu reprehenderit do. Ipsum cillum sit in et et sit minim enim reprehenderit deserunt. Fugiat reprehenderit magna in in magna velit eiusmod ea eiusmod consequat Lorem exercitation. Reprehenderit est est labore dolore Lorem dolore ex est irure eiusmod non proident.\r\nUllamco laboris sit enim sunt dolor. Cupidatat laborum dolor ea laboris pariatur. Et commodo fugiat est occaecat quis sint minim consectetur laborum aliqua amet sint et.\r\n"
+    },
+    {
+        "_id": "5efe362b6a310d6cba53ff5f",
+        "technology": "Grainspot",
+        "picture": "http://placehold.it/32x32",
+        "isActive": false,
+        "description": "Reprehenderit ad adipisicing veniam consectetur dolore cillum magna do labore proident excepteur. Deserunt minim nisi deserunt ullamco officia dolor aute aliqua nostrud d ?",
+        "answer": "Aute do elit eu velit ut laboris sit laboris labore ea laboris. Eiusmod eiusmod mollit elit in labore ex id minim dolor esse elit et. Dolore excepteur dolor do in culpa pariatur amet magna. Et ut sint laborum quis irure incididunt quis.\r\nMollit culpa est incididunt est aliqua excepteur elit sunt adipisicing eu duis esse veniam reprehenderit. Cupidatat laborum enim fugiat labore qui aute. Aliquip cillum irure magna ipsum qui sint pariatur veniam exercitation sint eu laborum amet in. Officia et est enim exercitation qui dolor consectetur veniam consectetur quis eu et dolore. Deserunt aliquip voluptate mollit ut magna aliqua.\r\n"
+    }
+]
+
+export default data;
