3ddecd02c8c8da2f931ff646edffe9aa52586366
Paulo Queiroz <phqs.phqs@gmail.com>
2020-07-03 12:09:29 -0300

    add new typography

--- applications/questions-and-answers/client/src/App.js
+++ applications/questions-and-answers/client/src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Questions and answers for interview / certification</h1>
       </header>
       <section className="container">
         <QuestionList questions={data} />
