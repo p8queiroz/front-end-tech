66b72b75230e3addd6166a6d743e5f853b2bf3da
Paulo Queiroz <phqs.phqs@gmail.com>
2020-07-06 09:43:04 -0300

    add new style to filters

--- applications/questions-and-answers/client/src/components/questionFilter/QuestionFilter.js
+++ applications/questions-and-answers/client/src/components/questionFilter/QuestionFilter.js
@@ -3,22 +3,26 @@ import React from 'react';
 const QuestionFilter = ({ callback }) => {
 
     return (
-        <div className="options">
-            <span>Technology</span>
-            <select id="technology" onChange={callback}>
-                <option value=""></option>
-                <option value="javascript">javascript</option>
-                <option value="react">React</option>
-                <option value="dotnet">.net</option>
-            </select>
-            <span>Level</span>
-            <select id="level" onChange={callback}>
-                <option value=""></option>
-                <option value="easy">easy</option>
-                <option value="medium">medium</option>
-                <option value="intermidiate">intermidiate</option>
-                <option value="hard">hard</option>
-            </select>
+        <div className="filters">
+            <div>
+                <span>Technology:</span>
+                <select id="technology" onChange={callback}>
+                    <option value=""></option>
+                    <option value="javascript">javascript</option>
+                    <option value="react">React</option>
+                    <option value="dotnet">.net</option>
+                </select>
+            </div>
+            <div>
+                <span>Level:</span>
+                <select id="level" onChange={callback}>
+                    <option value=""></option>
+                    <option value="easy">easy</option>
+                    <option value="medium">medium</option>
+                    <option value="intermidiate">intermidiate</option>
+                    <option value="hard">hard</option>
+                </select>
+            </div>
         </div >
     )
 };
