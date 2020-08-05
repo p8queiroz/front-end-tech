53946dde891ed4f7f5327c88337171214421accd
Paulo Queiroz <phqs.phqs@gmail.com>
2020-08-05 13:58:52 -0300

    send files

--- NodeJs/introduction/003 - express/002 - sendFiles/index.js
+++ NodeJs/introduction/003 - express/002 - sendFiles/index.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const app = express();
+const path = __dirname;
 
-app.get("/product/:category/:name", (req, res) => {
-    //call send method only once
-    res.send(req.params);
+app.get("/", (req, res) => {
+    res.sendFile(`${path}/files/index.html`);
 })
 
-
-app.get("/product/:status", (req, res) => {
-    res.send("product status ...");
+app.get("/about", (req, res) => {
+    res.sendFile(`${path}/files/about.html`);
 })
 
 app.listen(8081, (req, res) => {
-    console.log(`server is running or port 8081`)
+    console.log(`server is running or port 8081: ${path}`)
 })
