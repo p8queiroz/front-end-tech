abf76e6e462d9fc6c6dd7e0d88c281725d79de48
Paulo Queiroz <phqs.phqs@gmail.com>
2020-08-05 12:10:27 -0300

    add initial calculator to application

---
+++ NodeJs/introduction/calculator/main.js
@@ -0,0 +1,8 @@
+const Sum = require("./sum");
+const Diff = require("./diff");
+
+console.log("Welcome to my calculator:")
+console.log(Sum(3, 2))
+console.log(Diff(3, 2))
+console.log("Bye, see you in the next calculation")
+
