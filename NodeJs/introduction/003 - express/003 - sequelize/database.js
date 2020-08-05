5bca7bd472c51c35a962189a1b8b489fe3db4540
Paulo Queiroz <phqs.phqs@gmail.com>
2020-08-05 14:28:39 -0300

    conect database

---
+++ NodeJs/introduction/003 - express/003 - sequelize/database.js
@@ -0,0 +1,41 @@
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("sm_paulodev", "root", "vertrigo", {
+    host: "localhost",
+    dialect: "mysql"
+})
+
+
+const Post = sequelize.define(
+    'posts', {
+    Title: {
+        type: Sequelize.STRING
+    },
+    Content: {
+        type: Sequelize.STRING
+    },
+});
+
+//Post.sync({ force: true });
+Post.create({
+    Title: "Test",
+    Content: "Loren ispum tset lor vinitun"
+})
+
+const Comments = sequelize.define(
+    'comments', {
+    Content: {
+        type: Sequelize.STRING
+    },
+    PostId: {
+        type: Sequelize.INTEGER
+    },
+    UserId: {
+        type: Sequelize.INTEGER
+    },
+});
+
+Comments.sync({ force: true });
+
+sequelize.authenticate()
+    .then(() => console.log("*********logado********"))
+    .catch((error) => console.log("*********error********", error))
