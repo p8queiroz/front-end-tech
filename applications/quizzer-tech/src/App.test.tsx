d0c0f06e75711c9aef59d97c82bcf55d4bc1638a
Paulo Queiroz <phqs.phqs@gmail.com>
2020-09-24 12:17:46 -0300

    Question unit tests

--- applications/quizzer-tech/src/App.test.tsx
+++ applications/quizzer-tech/src/App.test.tsx
@@ -5,11 +5,6 @@ import { render, fireEvent, waitForElement } from '@testing-library/react'
 
 const renderComponent = () => render(<App />)
 
-// it('should render App', () => {
-//   const { getByText } = renderComponent()
-//   expect(getByText('Questions')).toBeInTheDocument()
-
-// });
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
--- applications/quizzer-tech/src/components/Question/QuestionList.test.tsx
+++ applications/quizzer-tech/src/components/Question/QuestionList.test.tsx
@@ -3,22 +3,21 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom'
 import QuestionList from "./QuestionList";
 import { ionFireEvent as fireEvent } from '@ionic/react-test-utils';
-// // extends
-// import 'react-testing-library/extend-expect';
-// interface ExtendedMatchers extends jest.Matchers<void> {
-//   toHaveTextContent: (htmlElement: string) => object;
-//   toBeInTheDOM: () => void;
-// }
+
 function mockFetch(data: any) {
     return jest.spyOn(window, 'fetch').mockResolvedValue(new Response(JSON.stringify(data)));
 }
 
-const renderComponent = (props: any) => render(<QuestionList {...props} />)
+const renderComponent = (props: any) => render(<QuestionList {...props} />);
 
 beforeEach(() => mockFetch([]));
 
 test('should render questions compoment', () => {
     const { getByText } = renderComponent({ questions: [], listType: 'all', hide: true })
-    //console.log(getByText('Quizzer App'));
     expect(getByText('Quizzer App')).toBeInTheDocument()
+});
+
+test('should render `No questions Found` when there is no question available', () => {
+    const { getByText } = renderComponent({ questions: [], listType: 'all', hide: false })
+    expect(getByText('No questions Found')).toBeInTheDocument()
 });
